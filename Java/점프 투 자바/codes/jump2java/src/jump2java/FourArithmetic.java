41613e09108cddf37fb2c4009066e8f8c2999675
lymchgmk <lymchgmk@gmail.com>
2021-11-07 05:23:33 +0900

    [Java][점프 투 자바][codes] 02장 자바 시작하기

---
+++ Java/점프 투 자바/codes/jump2java/src/jump2java/FourArithmetic.java
@@ -0,0 +1,27 @@
+package jump2java;
+
+public class FourArithmetic {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+		int i = 0;
+		int j = 10;
+		i++;
+		j--;
+		System.out.println(i);
+		System.out.println(j);
+		
+		int k1 = 0;
+		System.out.println(k1++);
+		System.out.println(k1);
+		
+		int k2 = 0;
+		System.out.println(++k2);
+		System.out.println(k2);
+	}
+}
