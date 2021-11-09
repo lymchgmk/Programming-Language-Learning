c58012a194466245cb0909bbbc93d49c6eb5d3df
lymchgmk <lymchgmk@gmail.com>
2021-11-09 09:07:16 +0900

    [Java][점프 투 자바] 08장 연습문제

---
+++ Java/점프 투 자바/codes/jump2java/src/jump2java/MultiplicationTable.java
@@ -0,0 +1,24 @@
+package jump2java;
+import java.util.Scanner;
+
+// 1. 2-9 사이 정수 입력 받기
+// 1-1. 범위 내의 정수가 아니면 오류 발생
+// 2. 특정 단 출력하기
+// 3. 새로 입력할지 종료할지 선택지 주기
+
+
+public class MultiplicationTable {
+	int base;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String inputLine;
+		int baseNum;
+		
+		System.out.print("몇 단을 출력할지 입력해주세요.");
+		inputLine = sc.nextLine();
+		// 
+		
+	}
+}
