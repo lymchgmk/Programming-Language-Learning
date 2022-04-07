9911b6b311a1c3362f985de7c1f3c39af5bcd419
lymchgmk <lymchgmk@gmail.com>
2022-04-07 15:27:14 +0900

    [Java][Java의 정석 3판] Chapter 06. 객체지향 프로그래밍 1

---
+++ Java/Java의 정석 3판/Chapter 06. 객체지향 프로그래밍 1/CardTest.java
@@ -0,0 +1,22 @@
+class CardTest {
+    public static void main(String[] args) {
+        System.out.println("Card.width = " + Card.width);
+        System.out.println("Card.height = " + Card.height);
+
+        Card c1 = new Card();
+        c1.kind = "Heart";
+        c1.number = 7;
+
+        Card c2 = new Card();
+        c2.kind = "Spade";
+        c2.number = 4;
+
+        System.out.println(c1.kind + " " + c1.number + " " + c1.width + " " + c1.height);
+        System.out.println(c2.kind + " " + c2.number + " " + c2.width + " " + c2.height);
+
+        c1.width = 50;
+        c1.height = 80;
+        System.out.println(c1.kind + " " + c1.number + " " + c1.width + " " + c1.height);
+        System.out.println(c2.kind + " " + c2.number + " " + c2.width + " " + c2.height);
+    }
+}
---
+++ Java/Java의 정석 3판/Chapter 06. 객체지향 프로그래밍 1/Tv.java
@@ -0,0 +1,17 @@
+class Tv {
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+
+    void channelUp() {
+        ++channel;
+    }
+
+    void channelDown() {
+        --channel;
+    }
+}
